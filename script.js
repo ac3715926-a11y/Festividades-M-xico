1391077b210725ca2f0dfd77980f38a58825a2f5
Álex Cruz <ac3715926@gmail.com>
2026-06-07 20:44:57 -0300

    Agregar index.html, styles.css y script.js para festividades de méxico

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function toggle(id){document.getElementById(id).classList.toggle("hidden");}
 function toggleDark(){document.body.classList.toggle("dark");}
-function toggleMusic(){let m=document.getElementById("music");m.paused?m.play():m.pause();}
-function quiz(){let r=document.getElementById("quiz").value;document.getElementById("resultado").innerText=r==800?"✅ Correcto":"❌ Incorrecto";}
-const els=document.querySelectorAll(".fade");window.addEventListener("scroll",()=>{els.forEach(e=>{if(e.getBoundingClientRect().top<window.innerHeight-100){e.classList.add("visible");}});});
+let music=document.getElementById("music");
+function toggleMusic(){music.paused?music.play():music.pause();}
+const els=document.querySelectorAll(".fade");
+window.addEventListener("scroll",()=>{els.forEach(e=>{if(e.getBoundingClientRect().top<window.innerHeight-100){e.classList.add("visible");}});});
